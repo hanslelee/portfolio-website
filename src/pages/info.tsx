2264fc3ad2eefec5bc89f0050dc4930a77b098b1
hanslelee <hssarah13@gmail.com>
2022-04-25 12:00:48 +0900

    Tagged Template Literal - Style component 정의, 사용

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
 import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
 
 type infoPageProps = {
     "data": {
@@ -24,14 +25,15 @@ const InfoPage: FunctionComponent<infoPageProps> = function ({
     return <div>
         <Global styles={globalStyle} />      
         <div css={TextStyle}>{title}</div>
-        {description} {author}
+        <Text1>{description}</Text1>
+        {author}
     </div>
 }
 
 export default InfoPage
 
 
-// Tagged Template Literal 방식으로 글로벌 스타일 정의
+// Tagged Template Literal 방식으로 css 스타일 정의
 const globalStyle = css`
     * {
         margin: 0;
@@ -45,6 +47,13 @@ const TextStyle = css`
     font-size: 18px;
     font-weight: 700;
     color: grey;
+`
+
+// Tagged Template Literal 방식을 통한 Styled Component 생성
+// 다른 요소의 스타일을 적용할땐 styled.img 처럼 해당 엘리먼트를 styled 뒤에 붙여 호출해주면 된다.
+const Text1 = styled.div`
+    font-size: 20px;
+    font-weight: 700;
 `
 
 export const metadataQuery = graphql `
