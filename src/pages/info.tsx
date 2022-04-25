c93eddc7cbb4c1240df76eb24fde5796bafefd35
hanslelee <hssarah13@gmail.com>
2022-04-25 11:52:55 +0900

    Tagged Template Literal - Global Css 정의, 적용

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import { Global, css } from '@emotion/react'
 
 type infoPageProps = {
     "data": {
@@ -21,11 +22,30 @@ const InfoPage: FunctionComponent<infoPageProps> = function ({
     },
 }) {
     return <div>
-        {title} {description} {author}
+        <Global styles={globalStyle} />      
+        <div css={TextStyle}>{title}</div>
+        {description} {author}
     </div>
 }
 
 export default InfoPage
+
+
+// Tagged Template Literal 방식으로 글로벌 스타일 정의
+const globalStyle = css`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 20px;
+    }
+`
+
+const TextStyle = css`
+    font-size: 18px;
+    font-weight: 700;
+    color: grey;
+`
 
 export const metadataQuery = graphql `
     {
